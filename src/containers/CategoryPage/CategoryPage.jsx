7abf381885a97c3aa285d885fae1f0dde1ad8c6f
Luong Tan Phat <tanphat9324@gmail.com>
2020-04-26 23:47:54 +0700

    create structure

---
+++ src/containers/CategoryPage/CategoryPage.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CategoryPage extends Component {
+    render() {
+        return (
+            <div>
+                CategoryPage
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {rootReducer} from './redux/reducers/index';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+const store = createStore(rootReducer,applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
